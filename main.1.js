8ba6b62bd50917ecca912401a203920215b57174
tumit <tumit@live.com>
2023-11-10 17:42:13 +0700

    feat: change init project

--- main.1.js
+++ main.1.js
@@ -1,36 +1,33 @@
-// * Mock data
-const mockData = [
-  { id: 1, name: 'Attack on Titan', star: 4 },
-  { id: 2, name: 'The Glory', star: 4 },
-  { id: 3, name: 'Forrest Gumpup', star: 2 },
-  { id: 4, name: 'Up', star: 5 },
-  { id: 5, name: 'Ted', star: 3 },
-  { id: 6, name: 'Madagascar', star: 3 }
-];
+// *
+// * Functions:
+// *
+function render(contents) {
 
-// * Functions: implement here
-//
+  // clear child
+  document.getElementById('list').innerHTML = ''
 
-// * UI: add ul & li
-// create ul
-const ul = document.createElement("ul");
+  // create ul
+  const ul = document.createElement("ul");
 
-// init contents
-const contents = mockData;
+  // loop contents
+  for (let content of contents) {
 
-// loop contents
-for (let content of contents) {
+    // create li
+    let li = document.createElement("li");
 
-  // create li
-  let li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(content.name + " : " + content.star + " stars")
+    );
 
-  li.appendChild(
-    document.createTextNode(content.name + " : " + content.star + " stars")
-  );
+    // append li to ul
+    ul.appendChild(li);
+  }
 
-  // append li to ul
-  ul.appendChild(li);
+  // append to parent node
+  document.getElementById('list').appendChild(ul);
 }
 
-// append ul to body
-document.body.appendChild(ul);
+// *
+// * Function Call:
+// *
+render(data);
